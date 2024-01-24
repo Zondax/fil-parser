476339cfeff675de119d5abeb5553346af9c4895
Lucas Francisco López <lucaslopezf@gmail.com>
2024-01-24 09:12:02 -0300

    Add omitempty tag to NodeInfo struct (#97)

--- types/node.go
+++ types/node.go
@@ -2,7 +2,7 @@ package types
 
 type NodeInfo struct {
 	// NodeFullVersion contains the node version from which this metadata was extracted
-	NodeFullVersion string `json:"node_full_version"`
+	NodeFullVersion string `json:"node_full_version,omitempty"`
 	// NodeMajorMinorVersion contains the node major.minor version from which this metadata was extracted
-	NodeMajorMinorVersion string `json:"node_major_minor_version"`
+	NodeMajorMinorVersion string `json:"node_major_minor_version,omitempty"`
 }
