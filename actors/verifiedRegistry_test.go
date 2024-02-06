7c54be4b0f19389c5a9dda2c8b8d5c07906b5b28
Lucas Francisco López <lucaslopezf@gmail.com>
2024-02-06 15:43:14 -0300

    fix createExternal decoding (#105)

--- actors/verifiedRegistry_test.go
+++ actors/verifiedRegistry_test.go
@@ -140,7 +140,7 @@ func TestActorParser_verifiedWithParamsAndReturn(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			rawParams, rawReturn, err := getParmasAndReturn(manifest.VerifregKey, tt.txType)
+			rawParams, rawReturn, err := getParamsAndReturn(manifest.VerifregKey, tt.txType)
 			require.NoError(t, err)
 			require.NotNil(t, rawParams)
 			require.NotNil(t, rawReturn)
