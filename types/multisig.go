10e6203d6c8fccc715bc00026eddbb240537070d
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2024-06-03 23:24:59 +0200

    remove unnecessary fields

--- types/multisig.go
+++ types/multisig.go
@@ -1,15 +1,13 @@
 package types
 
 type MultisigInfo struct {
-	ID                 string `json:"id"`
-	MultisigAddress    string `json:"multisig_address"`
-	Height             uint64 `json:"height"`
-	TxCid              string `json:"tx_cid"`
-	Signer             string `json:"signer"`
-	ActionType         string `json:"action_type"`
-	Value              string `json:"value"`
-	Direction          string `json:"direction"`
-	InteractingAddress string `json:"interacting_address"`
+	ID              string `json:"id"`
+	MultisigAddress string `json:"multisig_address"`
+	Height          uint64 `json:"height"`
+	TxCid           string `json:"tx_cid"`
+	Signer          string `json:"signer"`
+	ActionType      string `json:"action_type"`
+	Value           string `json:"value"`
 }
 
 type MultisigProposal struct {
