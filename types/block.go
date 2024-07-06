bb1fb5d9b6e770cdec35d4ae9993d2c11ddd8d09
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2024-07-06 18:21:41 +0200

    Delete unnecessary code

--- types/block.go
+++ types/block.go
@@ -2,21 +2,21 @@ package types
 
 type BasicBlockData struct {
 	// Height contains the block height
-	Height uint64 `json:"height" gorm:"index:idx_height"`
+	Height uint64 `json:"height"`
 	// TipsetHash contains the tipset hash
-	TipsetCid string `json:"tipset_cid" gorm:"index:idx_tipset_cid"`
+	TipsetCid string `json:"tipset_cid"`
 }
 
 type TxBasicBlockData struct {
 	BasicBlockData
 	// Block Cid
-	BlockCid string `json:"block_cid" gorm:"index:idx_blocks_cid"`
+	BlockCid string `json:"block_cid"`
 }
 
 type TipsetBasicBlockData struct {
 	BasicBlockData
 	// Blocks Cid
-	BlocksCid []string `json:"blocks_cid" gorm:"type:Array(String);index:idx_blocks_cid"`
+	BlocksCid []string `json:"blocks_cid"`
 
 	NodeInfo
 }
