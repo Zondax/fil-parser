893728b231bb5c9b2bca3f33df86855e835fe743
Julian Toledano <jtoledanodiaz@gmail.com>
2025-04-03 17:15:45 +0200

    update metrics

--- metrics/client.go
+++ metrics/client.go
@@ -7,8 +7,6 @@ var (
 	_ MetricsClient       = &Client{}
 )
 
-const metricLabelComponent = "component"
-
 // MetricsClient extends the TaskMetrics interface with additional functionality for registering custom metrics.
 // It provides a way to register custom metrics with labels and handlers while maintaining the base TaskMetrics capabilities.
 type MetricsClient interface {
@@ -25,32 +23,26 @@ type Metric struct {
 
 type Client struct {
 	metrics.TaskMetrics
-	component string
 }
 
-func NewMetricsClient(taskMetrics metrics.TaskMetrics, component string) *Client {
+func NewMetricsClient(taskMetrics metrics.TaskMetrics) *Client {
 	return &Client{
 		TaskMetrics: taskMetrics,
-		component:   component,
 	}
 }
 
 func NewNoopMetricsClient() *Client {
 	return &Client{
 		TaskMetrics: metrics.NewNoopMetrics(),
-		component:   "",
 	}
 }
 
 func (c *Client) RegisterCustomMetrics(customMetrics ...Metric) {
 	for _, metric := range customMetrics {
-		// TODO: do something with the errors
-		metric.Labels = append(metric.Labels, metricLabelComponent)
 		_ = c.RegisterMetric(metric.Name, metric.Help, metric.Labels, metric.Handler)
 	}
 }
 
 func (c *Client) IncrementMetric(name string, labels ...string) error {
-	labels = append(labels, c.component)
 	return c.TaskMetrics.IncrementMetric(name, labels...)
 }
