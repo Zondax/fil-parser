7f6d7c8a41636e87ec7c11622b523366bea74d3d
Eric Mokaya <emagembe@gmail.com>
2025-06-18 16:37:10 +0100

    fix: improve method name and comments

--- actors/address.go
+++ actors/address.go
@@ -9,14 +9,17 @@ import (
 	"github.com/zondax/golem/pkg/logger"
 )
 
-func ConsolidateRobustAddress(addr address.Address, h *helper.Helper, logger *logger.Logger, bestEffort bool) (string, error) {
+// ConsolidateToRobustAddress consolidates an address to a robust address
+// if the address is a zero address account actor, it returns the robust address of the zero address account actor
+// if the address is already a robust address, it returns the address
+// if the address is f2 evm, we consolidate f2 -> f0 -> f4
+func ConsolidateToRobustAddress(addr address.Address, h *helper.Helper, logger *logger.Logger, bestEffort bool) (string, error) {
 	actorCache := h.GetActorsCache()
-	isZeroAddressAccountActor, _, _ := h.IsZeroAddressAccountActor(addr)
+	if ok, _, _ := h.IsZeroAddressAccountActor(addr); ok {
+		return helper.ZeroAddressAccountActorRobust, nil
+	}
 
 	if isRobust, _ := common.IsRobustAddress(addr); isRobust {
-		if isZeroAddressAccountActor {
-			return helper.ZeroAddressAccountActorShort, nil
-		}
 		// we need to handle cases where a f2 address for evm actors is used
 		// f2 -> f0 -> f4, as we want to consolidate the address to f4 style
 		shortAddressStr, err := actorCache.GetShortAddress(addr)
@@ -30,9 +33,6 @@ func ConsolidateRobustAddress(addr address.Address, h *helper.Helper, logger *lo
 		return addr.String(), nil
 	}
 
-	if isZeroAddressAccountActor {
-		return helper.ZeroAddressAccountActorRobust, nil
-	}
 	robustAddress, err := actorCache.GetRobustAddress(addr)
 	if err != nil && !bestEffort {
 		logger.Warnf("Error converting address %s to robust format: %v", addr, err)
