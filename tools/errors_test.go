2f1b89a30ea1754e5b28a7b8064e7f8d988513fa
Eric Mokaya <emagembe@gmail.com>
2025-06-25 13:18:56 +0100

    test: update tests

--- tools/errors_test.go
+++ tools/errors_test.go
@@ -65,7 +65,7 @@ func TestCheckExitCodeCommonError(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := CheckExitCodeCommonError(tt.code.String()); got != tt.want {
+			if got := CheckExitCodeError(tt.code.String()); got != tt.want {
 				t.Errorf("CheckCheCommonError() = %v, want %v", got, tt.want)
 			}
 		})
