3d3cd3ece66d281e387bf67346fabaee200b432e
Eric Mokaya <emagembe@gmail.com>
2025-06-25 13:17:11 +0100

    feat: add all exit codes

--- tools/errors.go
+++ tools/errors.go
@@ -1,6 +1,8 @@
 package tools
 
-import "errors"
+import (
+	"errors"
+)
 
 var (
 	ErrUnknownMethod = errors.New("not known method")
@@ -11,7 +13,27 @@ var (
 
 type errorsMap map[string]string
 
+// https://github.com/filecoin-project/ref-fvm/blob/4eae3b6e8d1858abfdb82956dc8cbf082a0cac66/shared/src/error/mod.rs
 var commonErrors = errorsMap{
+
+	// System errors
+	"1":  "SysErrSenderInvalid",
+	"2":  "SysErrSenderStateInvalid",
+	"3":  "SysErrReserved1",
+	"4":  "SysErrIllegalInstruction",
+	"5":  "SysErrInvalidReceiver",
+	"6":  "SysErrInsufficientFunds",
+	"7":  "SysErrOutOfGas",
+	"8":  "SysErrReserved2",
+	"9":  "SysErrIllegalExitCode",
+	"10": "SysFatal",
+	"11": "SysErrMissingReturn",
+	"12": "SysErrReserved3",
+	"13": "SysErrReserved4",
+	"14": "SysErrReserved5",
+	"15": "SysErrReserved6",
+
+	// Common errors
 	"16": "ErrIllegalArgument",
 	"17": "ErrNotFound",
 	"18": "ErrForbidden",
@@ -22,10 +44,11 @@ var commonErrors = errorsMap{
 	"23": "ErrUnspecified",
 	"24": "ErrAssertionFailed",
 	"25": "ErrReadOnly",
+	"26": "ErrNotPayable",
 }
 
-// CheckExitCodeCommonError given an ExitCode.String() checks if is a common error
-func CheckExitCodeCommonError(code string) string {
+// CheckExitCodeError given an ExitCode.String() checks if is a common error
+func CheckExitCodeError(code string) string {
 	e, ok := commonErrors[code]
 	if !ok {
 		return code
