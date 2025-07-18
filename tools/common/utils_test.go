d02152cdc3eb771b8409a33e4ec9f2c2ed088ab2
Eric Mokaya <emagembe@gmail.com>
2025-07-18 17:12:43 +0100

    fix: test case

--- tools/common/utils_test.go
+++ tools/common/utils_test.go
@@ -79,7 +79,6 @@ func TestJsonEncodedBitfieldToSectorNumbers(t *testing.T) {
 	}{
 		{name: "test 1", bitField: []int{0, 4}, want: []uint64{0, 1, 2, 3}},
 		{name: "test 2", bitField: []int{0, 2, 1, 3}, want: []uint64{0, 1, 3, 4, 5}},
-		{name: "test 2", bitField: []int{0, 2}, want: []uint64{1, 3}},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
